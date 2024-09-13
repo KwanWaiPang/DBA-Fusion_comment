d1b0848eae8365df00958b8d11092ea40d370988
KwanWaiPang <gwpscut@163.com>
2024-09-13 13:05:49 +0800

    完成mono hku数据集（除vicon aggressive hdr外的验证）

--- batch_monohku.py
+++ batch_monohku.py
@@ -3,27 +3,27 @@ import subprocess
 
 # 遍历所有的序列
 for i in [\
-    'vicon_aggressive_hdr',\
-    'vicon_dark1',\
-    'vicon_dark2',\
-    'vicon_darktolight1',\
-    'vicon_darktolight2',\
     'vicon_hdr1',\
     'vicon_hdr2',\
     'vicon_hdr3',\
     'vicon_hdr4',\
+    'vicon_darktolight1',\
+    'vicon_darktolight2',\
     'vicon_lighttodark1',\
-    'vicon_lighttodark2'
+    'vicon_lighttodark2',\
+    'vicon_dark1',\
+    'vicon_dark2',\
+    'vicon_aggressive_hdr'
       ]:
-    p = subprocess.Popen("python demo_vio_davis240c.py" +\
-    " --imagedir=/media/lfl-data2/davis240c/%s/images_undistorted_left/"%i +\
-    " --imagestamp=/media/lfl-data2/davis240c/%s/raw_tss_imgs_ns_left.txt"%i +\
-    " --imupath=/media/lfl-data2/davis240c/%s/imu_data.csv"%i +\
-    " --gtpath=/media/lfl-data2/davis240c/%s/raw_gt_stamped_ns_left.txt"%i +\
-    " --resultpath=results/davis240c_%s.txt"%i +\
-    " --result_pdf=results/davis240c_%s.pdf"%i +\
+    p = subprocess.Popen("python demo_vio_mono_hku.py" +\
+    " --imagedir=/media/lfl-data2/Mono_HKU/%s/images_undistorted_davis346/"%i +\
+    " --imagestamp=/media/lfl-data2/Mono_HKU/%s/raw_tss_imgs_ns_davis346.txt"%i +\
+    " --imupath=/media/lfl-data2/Mono_HKU/%s/davis346_imu_data.csv"%i +\
+    " --gtpath=/media/lfl-data2/Mono_HKU/%s/raw_gt_stamped_us.txt"%i +\
+    " --resultpath=results/mono_hku_%s.txt"%i +\
+    " --result_pdf=results/mono_hku_%s.pdf"%i +\
     " --weights=/home/gwp/DBA-Fusion/droid.pth" +\
-    " --calib=/media/lfl-data2/davis240c/%s/calib_undist_left.txt"%i +\
+    " --calib=/media/lfl-data2/Mono_HKU/%s/calib_undist_davis346.txt"%i +\
     " --stride=2" +\
     " --max_factors=48" +\
     " --active_window=12" +\
